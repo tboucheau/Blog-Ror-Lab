a136d391df76f4787f58851d22fe3eaad7d573b4
tboucheau <opposite240@gmail.com>
2016-12-24 00:42:29 +0100

    home

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'categories/show'
-
-  get 'categories/new'
-
-  get 'categories/edit'
-
-  get 'categories/destroy'
-
-  get 'categories/update'
-
-  get 'categories/index'
-
     root to: 'pages#home'
-    get '/salut/(:name)', to: 'pages#salut', as: 'salut'
     resources :posts
     resources :categories
 end
