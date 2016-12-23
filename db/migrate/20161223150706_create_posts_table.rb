48fae54810f1e06623f4566962f4ad8d3764ea3d
tboucheau <opposite240@gmail.com>
2016-12-23 16:18:49 +0100

    long text on content

--- db/migrate/20161223150706_create_posts_table.rb
+++ db/migrate/20161223150706_create_posts_table.rb
@@ -2,7 +2,7 @@ class CreatePostsTable < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :content
+      t.text :content, limit: 4294967295
     end
   end
 end
