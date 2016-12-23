84c2c6dd90d889aca5680085cf855c274c362534
tboucheau <opposite240@gmail.com>
2016-12-23 14:50:21 +0100

    Initial commit : Ror with mysql test

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+
+Ma première application Ruby on Rails.
+Avec l'aide de codecademy.com et grafikart.fr
+
+Merci à eux
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+
+    root to: 'pages#home'
+
+    get '/salut/(:name)', to: 'pages#salut', as: 'salut'
+
+end
