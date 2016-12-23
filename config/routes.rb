af7e5a789c0b6e9eeecaf222193635ee7a18b333
tboucheau <opposite240@gmail.com>
2016-12-23 22:51:11 +0100

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-
-  get 'posts/index'
-
+    get 'posts/index'
     root to: 'pages#home'
-
     get '/salut/(:name)', to: 'pages#salut', as: 'salut'
-    get '/articles', to: 'posts#index', as: 'posts'
+    ressource :posts
 end
