c0546ea46d9920da26de567222f8f044982838fc
tboucheau <opposite240@gmail.com>
2016-12-23 22:51:57 +0100

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-    get 'posts/index'
+
     root to: 'pages#home'
     get '/salut/(:name)', to: 'pages#salut', as: 'salut'
-    ressource :posts
+    ressources :posts
 end
