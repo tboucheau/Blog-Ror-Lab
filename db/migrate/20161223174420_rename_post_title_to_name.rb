5f5926833dba6620edcd6b0580ecb009e3730ca5
tboucheau <opposite240@gmail.com>
2016-12-23 18:51:25 +0100

    add timestamp

--- db/migrate/20161223174420_rename_post_title_to_name.rb
+++ db/migrate/20161223174420_rename_post_title_to_name.rb
@@ -1,7 +1,8 @@
 class RenamePostTitleToName < ActiveRecord::Migration[5.0]
   def change
       change_table :posts do |t|
-      t.rename :title, :name
-      end
+          t.rename :title, :name
+          t.timestamps
+        end
   end
 end
