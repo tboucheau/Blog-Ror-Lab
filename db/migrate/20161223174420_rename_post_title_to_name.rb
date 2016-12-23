8b1c70291b02d336294e10fbab7265520f4370f7
tboucheau <opposite240@gmail.com>
2016-12-23 18:59:26 +0100

    remove timestamps

--- db/migrate/20161223174420_rename_post_title_to_name.rb
+++ db/migrate/20161223174420_rename_post_title_to_name.rb
@@ -2,7 +2,6 @@ class RenamePostTitleToName < ActiveRecord::Migration[5.0]
     def change
       change_table :posts do |t|
           t.rename :title, :name
-          t.timestamps
         end
     end
 end
