a1872ce17aefa8c7ca9047c013ae60cc1ec96716
tboucheau <opposite240@gmail.com>
2016-12-24 00:49:02 +0100

    add before action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,19 @@
 class PostsController < ApplicationController
+
+
+    before_action :set_posts, only: [:update, :edit, :show, :destroy]
+
     def index
-      @posts = Post.all
+        @posts = Post.all
     end
 
     def show
-        @post = Post.find(params[:id])
     end
 
     def edit
-        @post = Post.find(params[:id])
     end
 
     def update
-        @post = Post.find(params[:id])
         @post.update(post_params)
         redirect_to posts_path
     end
@@ -27,7 +28,6 @@ class PostsController < ApplicationController
     end
 
     def destroy
-        @post = Post.find(params[:id])
         @post.destroy
         redirect_to posts_path
     end
@@ -35,5 +35,9 @@ class PostsController < ApplicationController
     private
     def post_params
         params.require(:post).permit(:name, :content)
+    end
+
+    def set_posts
+        @post = Post.find(params[:id])
     end
 end
