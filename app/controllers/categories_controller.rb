f052c054b4f0b0315c6cc957e9837ee399679738
tboucheau <opposite240@gmail.com>
2016-12-24 00:00:02 +0100

    MVC category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,38 @@
 class CategoriesController < ApplicationController
-  def index
-  end
+
+    def index
+        @categories = Category.all
+    end
+
+    def show
+        @categories = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        @category.update(category_params)
+        redirect_to category_path(@category.id)
+    end
+    def new
+        @category = Category.new
+    end
+
+    def edit
+        @categories = Category.find(params[:id])
+    end
+
+    def create
+        @category = Category.create(category_params)
+        redirect_to category_path(@category.id)
+    end
+    def destroy
+        @category = Category.find(params[:id])
+        @category.destroy
+        redirect_to categories_path
+    end
+
+    private
+    def category_params
+        params.require(:category).permit(:name, :slug)
+    end
 end
