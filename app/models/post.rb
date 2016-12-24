5fa03606ae2337b11c04de97f806ab53671448a0
tboucheau <opposite240@gmail.com>
2016-12-24 02:13:42 +0100

    format slug validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
 
     validates :name, presence: true
     before_validation :default_slug
-
+    validates :slug, format: { with: /\A[a-z0-9\-]+\z/ }
 
     private
     def default_slug
