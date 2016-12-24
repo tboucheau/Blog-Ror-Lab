b77580b2b65922e28585c944f82524f1c807f96d
tboucheau <opposite240@gmail.com>
2016-12-24 09:53:38 +0100

    default value to conter_cache

--- db/migrate/20161224085233_add_posts_count_to_categories.rb
+++ db/migrate/20161224085233_add_posts_count_to_categories.rb
@@ -1,5 +1,5 @@
 class AddPostsCountToCategories < ActiveRecord::Migration[5.0]
   def change
-    add_column :categories, :posts_count, :integer
+    add_column :categories, :posts_count, :integer, default: 0
   end
 end
