14f489e4016194d3c84a837d82a5882324fefd74
tboucheau <opposite240@gmail.com>
2016-12-24 02:32:55 +0100

    module slug

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
     include Sluggable
+    before_validation :set_default_slug
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
 
     include Sluggable
     validates :name, presence: true
-    before_validation :default_slug
+    before_validation :set_default_slug
     validates :slug, format: { with: /\A[a-z0-9\-]+\z/ }, uniqueness: true
 
 end
