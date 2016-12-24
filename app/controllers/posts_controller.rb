4c6639c0086d87d67e66213a1d93aab1a4fa5d11
tboucheau <opposite240@gmail.com>
2016-12-24 02:08:46 +0100

    slug to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,7 +43,7 @@ class PostsController < ApplicationController
 
     private
     def post_params
-        params.require(:post).permit(:name, :content)
+        params.require(:post).permit(:name, :content, :slug)
     end
 
     def set_posts
