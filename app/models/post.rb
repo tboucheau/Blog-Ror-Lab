b9cdfc309106fb1e0e9435fcfb6b797ee8d95160
tboucheau <opposite240@gmail.com>
2016-12-24 02:12:33 +0100

    parameterize slug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < ApplicationRecord
 
     validates :name, presence: true
+    before_validation :default_slug
+
+
+    private
+    def default_slug
+        if !self.name.emtpy? && self.slug.empty?
+            self.slug = name.parameterize
+        end
+    end
 
 end
