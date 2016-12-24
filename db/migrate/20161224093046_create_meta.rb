8800d13a7e617ad734ca8a19ba7560252f124096
tboucheau <opposite240@gmail.com>
2016-12-24 10:33:20 +0100

    migration modifs

--- db/migrate/20161224093046_create_meta.rb
+++ db/migrate/20161224093046_create_meta.rb
@@ -1,6 +1,8 @@
 class CreateMeta < ActiveRecord::Migration[5.0]
   def change
-    create_table :meta do |t|
+    create_table :metas do |t|
+        t.string :name
+        t.references :metaable, polymorphic: true, index: true
     end
   end
 end
