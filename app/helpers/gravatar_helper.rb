6ab1ee0bb1c570c61771c4437a742d13401f43a9
tboucheau <opposite240@gmail.com>
2016-12-25 22:15:50 +0100

    api gravatar modif

--- app/helpers/gravatar_helper.rb
+++ app/helpers/gravatar_helper.rb
@@ -2,6 +2,6 @@ module GravatarHelper
 
     def gravatar(email, options = {s: 200})
         throw TypeError unless email.respond_to?(:to_s)
-        image_tag "https://gravatar.com/avatar/#{Digest::MD5.hexdigest(email.to_s)}?s=#{options[:s]}"
+        image_tag "https://fr.gravatar.com/userimage/#{Digest::MD5.hexdigest(email.to_s)}.jpg?size=#{options[:s]}"
     end
 end
