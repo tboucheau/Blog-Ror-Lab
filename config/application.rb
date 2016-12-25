cf8786ec49ba0642ddac3ebb53414ab74cfe4983
tboucheau <opposite240@gmail.com>
2016-12-25 22:23:18 +0100

    no helper auto generation

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+      config.autoload_paths << "#{Rails.root}/app/models/validators"
+      config.generators.helper = false
   end
 end
