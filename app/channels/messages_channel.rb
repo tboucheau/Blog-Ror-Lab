a638fe484c570b5a928fd8a13392dc2906cb0055
tboucheau <opposite240@gmail.com>
2016-12-26 23:44:47 +0100

    typo

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -4,6 +4,6 @@ class MessagesChannel < ApplicationCable::Channel
     end
 
     def speak(data)
-        ActionCable.server.broacast 'messages', message: data['message']
+        ActionCable.server.broadcast 'messages', message: data['message']
     end
 end
