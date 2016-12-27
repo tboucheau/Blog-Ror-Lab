27500be3098815bb1f80e3b4cfcfd716c1afbce5
tboucheau <opposite240@gmail.com>
2016-12-27 20:40:06 +0100

    no mails on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
     def home
-        ContactMailer.contact(Post.first).deliver_later
+        #ContactMailer.contact(Post.first).deliver_later
     end
 end
