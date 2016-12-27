2ceca002fe1a5f8512b52c878ab4bad7cd766cbf
tboucheau <opposite240@gmail.com>
2016-12-27 09:02:41 +0100

    test

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,4 @@ class MessagesController < ApplicationController
     def index
     end
 
-    def demo
-        ActionCable.server.broadcast('messages', {})
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
     get '/messages', to: 'messages#index'
-    get '/demo', to: 'messages#demo'
     root to: 'pages#home'
     resources :posts
     resources :categories
