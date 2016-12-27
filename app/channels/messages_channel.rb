72c88dc98248d7a3a510e28c34209e1a77112ec5
tboucheau <opposite240@gmail.com>
2016-12-27 08:17:27 +0100

    test

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -4,6 +4,6 @@ class MessagesChannel < ApplicationCable::Channel
     end
 
     def speak(data)
-      ActionCable.server.broadcast('messages', {'azeazeza'})
+      ActionCable.server.broadcast('messages', {})
     end
 end
